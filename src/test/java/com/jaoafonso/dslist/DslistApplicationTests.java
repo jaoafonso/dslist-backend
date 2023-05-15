11145e97c62c1f9b759dcf41aa74df4b4ebb7141
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-05-15 02:47:56 -0300

    Projeto iniciado

---
+++ src/test/java/com/jaoafonso/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaoafonso.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
