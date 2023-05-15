bed6b64a73b1e48936e354801ddc608249784fa3
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-05-15 17:27:21 -0300

    GameDTO e GET gameById implementados

--- src/main/java/com/jaoafonso/dslist/controllers/GameController.java
+++ src/main/java/com/jaoafonso/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.jaoafonso.dslist.controllers;
 
+import com.jaoafonso.dslist.dto.GameDTO;
 import com.jaoafonso.dslist.dto.GameMinDTO;
 import com.jaoafonso.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 }
--- src/main/java/com/jaoafonso/dslist/services/GameService.java
+++ src/main/java/com/jaoafonso/dslist/services/GameService.java
@@ -1,9 +1,11 @@
 package com.jaoafonso.dslist.services;
 
+import com.jaoafonso.dslist.dto.GameDTO;
 import com.jaoafonso.dslist.dto.GameMinDTO;
 import com.jaoafonso.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,7 +15,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        return new GameDTO(gameRepository.findById(id).get());
+        // Tratamento de exceções será implementado futuramente
     }
 }
